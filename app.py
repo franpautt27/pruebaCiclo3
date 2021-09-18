3d9a57598aa4cc5d2092d022fad11f681b3a99f4
Francisco Pautt <58583172+franpautt27@users.noreply.github.com>
2021-09-17 20:22:09 -0500

    codigo completo

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ numero = int(input("pasame un numero:  "))
 
 
 cont = 1
-for i in range(numero):
+for i in range(1, numero+1):
     if numero % i == 0:
         cont +=1
 
 if cont == 2:
     print("el numero", numero, " es primo")
+else:
+    print("el numero NO es primo")
